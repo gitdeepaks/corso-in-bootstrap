0648f5c5641e3baea1731cb05043a20c2b91b14b
gitdeepaks <deepaksankhyan92@outlook.com>
2023-07-04 14:53:35 +0530

    new commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // Add custom JavaScript here
+// User Scroll For Navbar
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-dark");
+    } else {
+      navbar.classList.remove("bg-dark");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", userScroll);
